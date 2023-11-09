3f6819ecd626bf21308ca2351ff90d75b7eaafd9
Muhamad Haekal Syukur <147034335+Haekalss@users.noreply.github.com>
2023-11-09 19:24:21 +0700

    Assessment haekal

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-![Disini menampikan sebuah microfrontend e-commerce sederhana](Gambar/Screenshot/Screenshot(29).png) 
-Dengan menekan Button Sneakers, Loafers, Slip on, dan Boots
-(Gambar/Screenshot/Screenshot(30).png)
-Muncul perubahan paragraf menggunakan Javascript seperti gambar di atas
-```
+function paragraf0() {
+  document.getElementById("sepatu").innerHTML = "";
+}
 function paragraf1() {
   document.getElementById("sepatu").innerHTML =
     "Sneakers adalah sepatu yang biasanya dirancang untuk olahraga atau bentuk kegiatan fisik lainnya selain juga banyak dipakai untuk kegiatan sehari-hari. Sepatu kets mula-mula dipopulerisasikan oleh perusahaan-perusahaan seperti Converse, Nike, dan Spalding pada pertengahan abad ke-20.";
@@ -19,10 +17,3 @@ function paragraf4() {
   document.getElementById("sepatu").innerHTML =
     "Sepatu bot adalah sejenis alas kaki. Mayoritas sepatu bot menutupi bagian telapak kaki sampai ke bagian mata kaki. Sebagian sepatu bot juga menutup bagian betis dan bahkan ada juga yang sampai ke pinggang. Pada zaman dahulu sepatu bot biasanya terbuat dari kulit dan karet.";
 }
-```
-Selanjutnya dengan menekan button Home kembali lagi ke awal paragraf hilang
-(Gambar/Screenshot/Screenshot(31).png)
-```
-function paragraf0() {
-  document.getElementById("sepatu").innerHTML = "";
-}```
