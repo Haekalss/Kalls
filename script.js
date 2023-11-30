b133ca2a8977bf7233ab5576c3f5582a7beaf158
Muhamad Haekal Syukur <147034335+Haekalss@users.noreply.github.com>
2023-11-30 14:50:10 +0700

    nb6

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function paragraf0() {
 }
 
 function paragraf1() {   
-  document.getElementById("image1").src = "nb1.png"
+  document.getElementById("image1").src = "nb1.png" onclick('alertshow');
   document.getElementById("image2").src = "nb2.png"
   document.getElementById("image3").src = "nb3.png"
   document.getElementById("image4").src = "nb4.png"
